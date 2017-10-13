3b25a92c82c3bce02c1063a9fab83afdf5aec89c
loconluis <luis.locon@gmail.com>
2017-10-12 22:02:27 -0600

    Manejando errores

--- app.js
+++ app.js
@@ -14,24 +14,29 @@ const argv = yargs
   .alias('help', 'h')
   .argv
 
-console.log(argv.address)
-
 const endPoint = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 let encodeAddress = encodeURIComponent(argv.address)
 
 const apiURL = endPoint + encodeAddress
-console.log(apiURL)
 
 request({
   url: apiURL,
   json: true
 }, (err, response, body) => {
-  if (err) { }
-  // console.log(JSON.stringify(body.geometry.location, undefined, 2))
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`)
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`)
+  if (err) {
+    // si existe el error
+    console.log('Unable to connect to the server')
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.')
+  } else if (body.status === 'OK') {
+    console.log('Fetching...')
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`)
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`)
+  }
 })
 
+// body.status = devuelve si la consulta fue exitosa o fallida
+// console.log(JSON.stringify(body.geometry.location, undefined, 2))
 // body.results[0].geometry.location.lat
 // body.results[0].geometry.location.lng
